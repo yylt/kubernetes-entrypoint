a189d999930ed699712daa16f08054dd458b9360
yylt <yang8518296@163.com>
2023-07-12 11:35:09 +0800

    add kubeconfig option

--- kubernetes-entrypoint.go
+++ kubernetes-entrypoint.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	_ "opendev.org/airship/kubernetes-entrypoint/dependencies/config"
@@ -15,13 +16,30 @@ import (
 	"opendev.org/airship/kubernetes-entrypoint/logger"
 	command "opendev.org/airship/kubernetes-entrypoint/util/command"
 	"opendev.org/airship/kubernetes-entrypoint/util/env"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	var comm []string
 	var entrypoint *entry.Entrypoint
+	var resetconfig *rest.Config
 	var err error
-	if entrypoint, err = entry.New(nil); err != nil {
+	kc := flag.String("kubeconfig", "", "kubeconfig file path")
+	flag.Parse()
+	if kc != nil && *kc != "" {
+		content, err := os.ReadFile(*kc)
+		if err != nil {
+			logger.Warning.Printf("read file %s failed: %s. use incluster config", *kc, err)
+		} else {
+			resetconfig, err = clientcmd.RESTConfigFromKubeConfig(content)
+			if err != nil {
+				logger.Warning.Printf("parse kubeconfig file failed: %s. use incluster config", err)
+			}
+		}
+	}
+	if entrypoint, err = entry.New(resetconfig); err != nil {
 		logger.Error.Printf("Creating entrypoint failed: %v", err)
 		os.Exit(1)
 	}
